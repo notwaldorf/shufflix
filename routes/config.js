fd737e87ce121246be7a6a7d5542c691babbbf3a
monica <notwaldorf@gmail.com>
2013-01-21 00:38:58 -0500

    heroku setup

--- routes/config.js
+++ routes/config.js
@@ -1,6 +1,11 @@
 var config = {}
 
-config.apiKey = 'batman';			// <---- fill this in unless you're batman
-config.apiSecret = 'batman';		// <---- this one too
+// if you're running this locally
+//config.apiKey = 'batman';			// <---- fill this in unless you're batman
+//config.apiSecret = 'batman';		// <---- this one too
+
+// alternatively, if you're running this from heroku:
+config.apiKey = process.env.API_KEY
+config.apiSecret = process.env.API_SECRET
 
 module.exports = config;
